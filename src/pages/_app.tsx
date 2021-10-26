0d53b22a415e002c2d2ef41c6bb30b5f9d7255f0
Valentin Hervieu <valentin.hervieu@ricardo.ch>
2021-10-26 12:42:49 +0200

    Cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,30 +3,37 @@ import '../global.css'
 import { AppProps } from 'next/app'
 import * as React from 'react'
 import Head from 'next/head'
+import ReactGA from 'react-ga'
 
-import Analytics from '#src/components/Analytics'
 import Header from '#src/components/Header'
 import Content from '#src/components/Content'
 import Footer from '#src/components/Footer'
 import colors from '#src/components/../const/colors'
 import config from '#src/const/config'
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <title>{config.title}</title>
-      <meta name="description" content="Personal website of Valentin Hervieu" />
-      <meta name="keywords" content="Valentin, Hervieu, React, Frontend" />
-      <link rel="icon" type="image/png" href="/favicon.png" />
-      <meta name="theme-color" content={colors.primaryColor} />
-    </Head>
-    <Analytics />
-    <Header siteTitle={config.title} />
-    <Content>
-      <Component {...pageProps} />
-    </Content>
-    <Footer />
-  </>
-)
-
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  React.useEffect(() => {
+    ReactGA.initialize('UA-40453888-1')
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, [])
+  return (
+    <>
+      <Head>
+        <title>{config.title}</title>
+        <meta
+          name="description"
+          content="Personal website of Valentin Hervieu"
+        />
+        <meta name="keywords" content="Valentin, Hervieu, React, Frontend" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta name="theme-color" content={colors.primaryColor} />
+      </Head>
+      <Header siteTitle={config.title} />
+      <Content>
+        <Component {...pageProps} />
+      </Content>
+      <Footer />
+    </>
+  )
+}
 export default MyApp
