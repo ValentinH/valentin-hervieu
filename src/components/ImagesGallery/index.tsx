88b6b5b3c429d081511781634ca4a6971d4adf43
Valentin Hervieu <valentin.hervieu@ricardo.ch>
2021-10-26 17:10:21 +0200

    Improve gallery thumbnails

--- src/components/ImagesGallery/index.tsx
+++ src/components/ImagesGallery/index.tsx
@@ -1,4 +1,5 @@
 import 'react-image-gallery/styles/css/image-gallery.css'
+import { ClassNames } from '@emotion/react'
 import Image from 'next/image'
 import React from 'react'
 import ImageGallery, { ReactImageGalleryItem } from 'react-image-gallery'
@@ -13,40 +14,49 @@ const ImagesGallery = ({ images }: Props) => {
   return (
     <div>
       <h2>Some pictures from my camera</h2>
-      <ImageGallery
-        items={images.map((img) => ({
-          ...img,
-        }))}
-        showFullscreenButton={false}
-        renderItem={(item) =>
-          item.original && (
-            <div
-              css={{
-                paddingBottom: '56.25%', // 16/9 ratio
-                background: '#121212',
-                position: 'relative',
-              }}
-            >
-              <Image
-                src={item.original}
-                layout="fill"
-                priority={item === images[0]}
-                alt={item.originalAlt}
-              />
-            </div>
-          )
-        }
-        renderThumbInner={(item) =>
-          item.thumbnail && (
-            <Image
-              src={item.thumbnail}
-              width={92}
-              height={52}
-              alt={item.thumbnailAlt}
-            />
-          )
-        }
-      />
+      <ClassNames>
+        {({ css }) => (
+          <ImageGallery
+            items={images.map((img) => ({
+              ...img,
+            }))}
+            showFullscreenButton={false}
+            additionalClass={css({
+              '& .image-gallery-thumbnail': {
+                display: 'inline-flex',
+              },
+            })}
+            renderItem={(item) =>
+              item.original && (
+                <div
+                  css={{
+                    paddingBottom: '56.25%', // 16/9 ratio
+                    background: '#121212',
+                    position: 'relative',
+                  }}
+                >
+                  <Image
+                    src={item.original}
+                    layout="fill"
+                    priority={item === images[0]}
+                    alt={item.originalAlt}
+                  />
+                </div>
+              )
+            }
+            renderThumbInner={(item) =>
+              item.thumbnail && (
+                <Image
+                  src={item.thumbnail}
+                  width={92}
+                  height={55}
+                  alt={item.thumbnailAlt}
+                />
+              )
+            }
+          />
+        )}
+      </ClassNames>
     </div>
   )
 }
