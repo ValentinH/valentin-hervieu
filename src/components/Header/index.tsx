72a4f8114b99a73b2be6e673bab9d18363272a08
Valentin Hervieu <valentin@hervi.eu>
2026-05-06 15:44:55 +0200

    fix title alignment

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,7 +16,7 @@ const Header = ({ siteTitle }: Props) => (
       'relative z-0 flex h-[30vw] max-h-[300px] min-h-[180px] flex-col items-center justify-center overflow-hidden text-white',
     )}
   >
-    <Typography as="h1" variant="heroTitle" color="white">
+    <Typography as="h1" variant="heroTitle" color="white" className="text-center">
       <Link className="text-white no-underline" to="/">
         {siteTitle}
       </Link>
